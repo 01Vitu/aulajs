7f0dc90bc4989b1b134bdb422977f35237967fe8
Victor <victor.oliveira2049b@gmail.com>
2022-07-29 16:13:56 -0300

    testes js

---
+++ js/main.js
@@ -0,0 +1,151 @@
+
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>"
+    
+
+
+    //alert("Obrigado por clicar")
+}
+
+function redirecionar(){
+  window.open("https://www.youtube.com/")
+ // window.location.href="https://www.youtube.com/"
+
+}
+function trocar(elemento){
+ // document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse"
+ elemento.innerHTML = "Obrigado por passar o mouse"
+ // alert ("trocar texto")
+}
+function voltar(elemento){
+  elemento.innerHTML = "Passe o mouse aqui"
+  //document.getElementById("mousemove").innerHTML="passe o mouse aqui"
+}
+
+function load(){
+
+alert("PAGINA CARREGADA")
+}
+
+function funcaoChange(elemento){
+
+  console.log(elemento.value)
+}
+//function soma(n1,n2) {
+  //  return n1+n2
+    
+//}
+//function validaIdade(idade){
+  //  var validar
+    //    if(idade>=18){
+      //  validar=true
+    //}
+      //  else{
+    //validar=false
+    
+//}
+//return validar
+//}
+//var idade = prompt("Qual sua idade")
+//console.log(validaIdade(idade))
+
+
+
+
+
+
+
+//var d = new Date()
+//alert(d.getMonth())
+//alert(d.getFullYear())
+
+
+
+//var count
+//for(count = 0; count <=5; count++){
+ //   alert(count)
+//}
+
+
+
+
+
+
+
+
+//var count=0
+//while(count<5){
+  //  console.log(count)
+    //alert(count)
+    //count++
+//}
+
+
+
+//var idade= prompt("Qual sua idade")
+
+//if(idade>=18){
+  //  alert("Maior de idade")
+//}
+//else{
+ //   alert("Menor de idade")
+
+//}
+
+
+
+
+
+
+
+
+//var frutas =[{nome: "Maça", cor: "Vermelha"},{nome:"uva", cor:"Roxa"}]
+//console.log(frutas);
+//alert (frutas[1].nome)
+
+
+
+
+//var fruta ={nome: "Maça", cor: "Vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor)
+
+
+
+
+
+
+
+
+
+//var lista=["Maça", "Pera", "Laranja"]
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista.reverse());
+//console.log(lista.toString())
+//console.log(lista.join(" "))
+
+
+
+
+
+
+
+
+
+
+//var nome ="Victor";
+//var idade = 20;
+//var idade2 = 19;
+//var frase="Japao é o melhor time do mundo"
+
+//alert(idade+idade2);
+//console.log(nome);
+//console.log(idade+idade2)
+//console.log(frase.replace("Japao", "Brasil"))
+//alert(frase.replace("Japao", "Brasil"))
+
+
+
